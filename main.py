c845e527588dc2717686f71f23696ae4e15dbaec
LeviSecula <levi.secula@students.fhu.edu>
2024-02-06 09:20:17 -0600

    Rewrote it all
    Couldn't get it to function properly, so I condensed the month function into a dictionary rather than an if function, I added the parse_day function to help process the comma, and rewrote several bits but it works now.

--- main.py
+++ main.py
@@ -2,54 +2,36 @@
 #as a string
 #January -> 1 (as a string)
 def parse_month(month):
-    if month == "January":
-        return "01"
-    if month == "February":
-        return "02"
-    if month == "March":
-        return "03"
-    if month == "April":
-        return "04"
-    if month == "May":
-        return "05"
-    if month == "June":
-        return "06"
-    if month == "July":
-        return "07"
-    if month == "August":
-        return "08"
-    if month == "September":
-        return "09"
-    if month == "October":
-        return "10"
-    if month == "November":
-        return "11"
-    if month == "December":
-        return "12"
+    months = {
+        "January": "01", "February": "02", "March": "03", "April": "04",
+        "May": "05", "June": "06", "July": "07", "August": "08",
+        "September": "09", "October": "10", "November": "11",
+        "December": "12"
+    }
+    return months.get(month, "That's Not Right")
 
+def parse_day(day):
+    day = day.replace(",", "")
+    return f"{int(day):02d}"
 
-
-def FixDayCode(day):
-    fatzero = "0"
-    if day < 10:
-        fatzero += str(day)
-    else:
-        fatzero = str(day)
-    return fatzero
+def parse_date(user_string):
+    components = user_string.split()
     
-def parse_date(RawInput):
-    parts = RawInput.split()
-    month = parse_month(parts[0])
-    day = FixDayCode(int(parts[1]))
-    year = parts[2]
-    return f"{month}/{day}/{year}"
+    if len(components) == 3:
+        month = parse_month(components[0])
+        day = parse_day(components[1])
+        year = components[2]
+        return f"{month}/{day}/{year}"
+    else:
+        return "Oopsie Daisy, that doesn't look right"
 
 if __name__ == '__main__':
-    RawInput = input()
-    while RawInput != "-1":
-        parsed = parse_date(RawInput)
+    raw_input = input()
+
+    while raw_input != "-1":
+        parsed = parse_date(raw_input)
         print(parsed)
-        RawInput = input()
+        raw_input = input()
 
     
  
