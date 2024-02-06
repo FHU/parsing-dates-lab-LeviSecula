ae82a94d63eb6f24843d426894457fa080f012d5
LeviSecula <levi.secula@students.fhu.edu>
2024-02-06 09:11:47 -0600

    Working hard or Hardly working
    hardly working

--- main.py
+++ main.py
@@ -2,12 +2,54 @@
 #as a string
 #January -> 1 (as a string)
 def parse_month(month):
-    pass
+    if month == "January":
+        return "01"
+    if month == "February":
+        return "02"
+    if month == "March":
+        return "03"
+    if month == "April":
+        return "04"
+    if month == "May":
+        return "05"
+    if month == "June":
+        return "06"
+    if month == "July":
+        return "07"
+    if month == "August":
+        return "08"
+    if month == "September":
+        return "09"
+    if month == "October":
+        return "10"
+    if month == "November":
+        return "11"
+    if month == "December":
+        return "12"
 
-#parse_date function should return the date formated as MM/DD/YYYY
-def parse_date(user_string):
-    pass
 
-#REMOVE PASS AND YOUR CODE GOES HERE
+
+def FixDayCode(day):
+    fatzero = "0"
+    if day < 10:
+        fatzero += str(day)
+    else:
+        fatzero = str(day)
+    return fatzero
+    
+def parse_date(RawInput):
+    parts = RawInput.split()
+    month = parse_month(parts[0])
+    day = FixDayCode(int(parts[1]))
+    year = parts[2]
+    return f"{month}/{day}/{year}"
+
 if __name__ == '__main__':
-    pass
+    RawInput = input()
+    while RawInput != "-1":
+        parsed = parse_date(RawInput)
+        print(parsed)
+        RawInput = input()
+
+    
+ 
